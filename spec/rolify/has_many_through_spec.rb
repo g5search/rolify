c015ea3ca94883e6af177b379d050814a27e9c7a
maeve <maeve.revels@getg5.com>
2015-07-21 23:07:29 +0000

    Initial test case for has_many :through

---
+++ spec/rolify/has_many_through_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+require "rolify/shared_examples/shared_examples_for_roles"
+require "rolify/shared_examples/shared_examples_for_dynamic"
+require "rolify/shared_examples/shared_examples_for_scopes"
+require "rolify/shared_examples/shared_examples_for_callbacks"
+
+describe "Using Rolify with has_many :through relationship" do
+  def user_class
+    Employee
+  end
+
+  def role_class
+    Permission
+  end
+
+  it_behaves_like Rolify::Role
+  it_behaves_like "Role.scopes"
+  it_behaves_like Rolify::Dynamic
+  it_behaves_like "Rolify.callbacks"
+end
