d0af22dd351b3ff8480efca08577f6435befa0f5
maeve <maeve.revels@getg5.com>
2015-07-22 19:25:02 +0000

    Replace def with let for shared examples

--- spec/rolify/custom_spec.rb
+++ spec/rolify/custom_spec.rb
@@ -5,14 +5,9 @@ require "rolify/shared_examples/shared_examples_for_scopes"
 require "rolify/shared_examples/shared_examples_for_callbacks"
 
 describe "Using Rolify with custom User and Role class names" do
-  def user_class
-    Customer
-  end
+  let(:user_class) { Customer }
+  let(:role_class) { Privilege }
 
-  def role_class
-    Privilege
-  end
-  
   it_behaves_like Rolify::Role
   it_behaves_like "Role.scopes"
   it_behaves_like Rolify::Dynamic
--- spec/rolify/namespace_spec.rb
+++ spec/rolify/namespace_spec.rb
@@ -5,18 +5,10 @@ require "rolify/shared_examples/shared_examples_for_scopes"
 require "rolify/shared_examples/shared_examples_for_callbacks"
 
 describe "Rolify.namespace" do
-  def user_class
-    Admin::Moderator
-  end
+  let(:user_class) { Admin::Moderator }
+  let(:role_class) { Admin::Right }
+  let(:join_table) { "moderators_rights" }
 
-  def role_class
-    Admin::Right
-  end
-  
-  def join_table
-    "moderators_rights"
-  end
-  
   it_behaves_like Rolify::Role
   it_behaves_like "Role.scopes"
   it_behaves_like Rolify::Dynamic
